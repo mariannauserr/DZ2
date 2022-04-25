407113e1036b2354cc8aa308d5dfa369fcc83977
Maria Negretskaya <marian.93@mail.ru>
2022-04-25 19:47:18 +0300

    Miles for one ticket

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 80;
+        int mile = 20;
+
+        System.out.println("Количество начисленных миль за купленный билет");
+        System.out.println((ticket / mile) + " миль ");
+    }
+}
